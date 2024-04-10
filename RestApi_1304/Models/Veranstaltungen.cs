02778345770757bae2a7aa31650ef9eccb8177e5
Gabriel Antonio Bischof <G.Bischof.inf22@stud.bbbaden.ch>
2024-04-10 12:02:51 +0200

    Changes

--- RestApi_1304/Models/Veranstaltungen.cs
+++ RestApi_1304/Models/Veranstaltungen.cs
@@ -4,8 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public DateOnly Datum { get; set; }
-
+        public DateTime Datum { get; set; }
         public string Ort { get; set; }
 
     }
